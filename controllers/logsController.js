3a71a84a807fb594dafd2b52108ed1f7e287df02
CSAN7690 <christophersanchez@pursuit.org>
2024-06-25 15:35:11 -0400

    Passed all test + added Routes for Update & Delete ✨

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -24,4 +24,25 @@ router.post('/', (req, res) => {
     res.status(303).location('/logs').json(newLog);
 });
 
+// Update Route
+router.put('/', (req, res) => {
+    const logIndex = req.params.id
+    if (logs[logIndex]) {
+        res.status(303).location('./logs').json(logs[logIndex])
+    } else {
+        res.redirect('404')
+    }
+})
+
+// Delete Route
+router.delete('/:id', (req, res) => {
+    const logIndex = req.params.id
+    if (logs[logIndex]) {
+        logs.splice(logIndex, 1)
+        res.status(303).location('/logs').json(logs)
+    } else {
+        res.redirect('404')
+    }
+})
+
 module.exports = router;
