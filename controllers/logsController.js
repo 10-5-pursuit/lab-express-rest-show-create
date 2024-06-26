6d085d1806b54a9e5452511104622357065b11b9
marinn6 <nmarin@pursuit.org>
2024-06-26 15:26:45 -0400

    Delete section complete

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -7,6 +7,22 @@ const logsArray = require('../models/log')
 //Index Route
 //represents localhost:4006/logs/
 logs.get('/', (req, res) => {
+    const { order, mistakes, lastCrisis } = req.query
+    console.log(order)
+    if(order === "asc"){
+    logsArray.sort((a, b) => 
+         a.captainName.localeCompare(b.captainName) 
+        )
+    } else if (order === "desc"){
+        logsArray.sort((a,b) => 
+        b.captainName.localeCompare(a.captainName)
+        )
+    }
+
+    if(mistakes === "true"){
+        logsArray.filter()
+    }
+    //console.log(req.query, order)
     res.json(logsArray)
 })
 
@@ -26,6 +42,22 @@ logs.post('/', (req, res) => {
     res.json(logsArray[logsArray.length - 1])
 })
 
-//Create Route
+//Delete
+logs.delete('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params
+    if(logsArray[arrayIndex]) {
+        const deletedLogs = logsArray.splice(arrayIndex, 1)
+        res.json(deletedLogs)
+    } else {
+        res.json({error:"Log Not Found"})
+    }
+})
+
+//PUT = UPDATE
+logs.put("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params
+    logsArray[arrayIndex] = req.body
+    res.status(218).json(logsArray[arrayIndex])
+})
 
 module.exports = logs
