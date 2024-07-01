1134409fe2d97bc8263ec48f0e235a26a9e418cf
nmatos <nmatos@pursuit.org>
2024-07-01 18:02:59 -0400

    completed lab

--- controllers/captainsController.js
+++ controllers/captainsController.js
@@ -8,16 +8,32 @@ captains.get('/', (req, res) => {
 
 captains.get('/:arrayIndex', (req, res) => {
     const { arrayIndex } = req.params;
-    if(captainsArray[arrayIndex]){
-        res.status(200).json(captainsArray[arrayIndex])
+    if(captainsArray[arrayIndex]) {
+        res.status(200).json(captainsArray[arrayIndex]);
     } else {
         res.redirect('/*');
-    }
-})
+    };
+});
 
 captains.post('/', (req, res) => {
     captainsArray.push(req.body)
     res.json(captainsArray[captainsArray.length - 1]);
-})
+});
+
+captains.delete('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    if(captainsArray[arrayIndex]) {
+        const deletedCaptain = captainsArray.splice(arrayIndex, 1);
+        res.json(deletedCaptain[0]);
+    } else {
+        res.redirect('/*');
+    };
+});
+
+captains.put('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    captainsArray[arrayIndex] = req.body;
+    res.status(200).json(captainsArray[arrayIndex]);
+});
 
 module.exports = captains;
