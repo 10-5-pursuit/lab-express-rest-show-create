bb95df662b7baf47a002b1e85a00ab35717ef651
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-02 02:29:41 -0400

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const personelController = require('./controllers/shipPersonelController');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/personel', personelController);
